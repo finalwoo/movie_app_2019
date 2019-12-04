53f50fe026d7db93e6f80836905436928e3e7b7b
finalwoo <finalwoo@ddnayo.com>
2019-12-04 14:23:44 +0900

    component life cycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import PropTypes from "prop-types";
 
 // state 는 오브젝트이고 component의 data를 넣을 공간이며 data는 변함
 class App extends React.Component{
+    constructor(props){ // 시작전 호출되며 super 를 사용해야함. 리액트가 아닌 자바스크립트임
+        super(props)
+        console.log("1.constructor() : render 전에 실행됨"); 
+    }
     state = {
         count: 0 // 바뀔데이터
     }
@@ -12,7 +16,17 @@ class App extends React.Component{
     minus = () => {
         this.setState({count:this.state.count - 1}) // not good
     };
+    componentDidMount(){
+        console.log("3.componentDidMount() : component rendered reder() 이후 호출")
+    }
+    componentDidUpdate(){
+        console.log("4.componenetDidUpdate() : 업데이트가 되면 실행")
+    }
+    componentWillUnmount() {
+        console.log("5.componenetWillUnmount() : 컴포넌트가 끝날때 실행됨")
+    }
     render(){
+        console.log("2.render() : i'm rendering")
         return <div>
                 <h1>The count number is : {this.state.count}</h1>
                 <button onClick={this.add}>Add</button>
